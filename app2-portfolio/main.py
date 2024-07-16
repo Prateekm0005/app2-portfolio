27d7698f9f5c2b6f6c2e399234662b9a0d20a02e
Prateek <prateekmahajan03@gmail.com>
2024-07-16 21:03:07 +0530

    Add discriptions to the new column

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/profile.jpeg", width=650)
+
+with col2:
+    st.title("Prateek Mahajan")
+    content = """
+    Hi, My name is Prateek Mahajan, and I am currently pursuing a Bachelor of Technology (B.Tech) in Computer Science. My journey in this field has been driven by a profound interest in technology and its potential to transform the world. Through my academic experiences, I have developed a strong foundation in various programming languages, algorithms, and software development practices.
+    Beyond my academic pursuits, I have a deep passion for sports. Engaging in different sports activities not only helps me stay physically fit but also teaches me valuable life skills such as teamwork, leadership, and perseverance. Balancing my studies with sports has instilled in me a sense of discipline and time management, which I believe are crucial for success in any field.
+    """
+    st.info(content)
+
+content2 = """
+        Below you can find of some of the Apps I have build in python.Feel free to contact me!
+        """
+st.write(content2)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
