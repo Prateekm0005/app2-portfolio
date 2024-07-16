e0e8973588b004c2285083635f41f5fdb81b6caf
Prateek <prateekmahajan03@gmail.com>
2024-07-16 21:20:33 +0530

    Render the titles into two columns

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -20,6 +21,16 @@ content2 = """
         """
 st.write(content2)
 
+df = pandas.read_csv("venv/data.csv", sep=";")
 
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
 
 
