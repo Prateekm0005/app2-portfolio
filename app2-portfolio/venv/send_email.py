355487589f8a6cafc1c3e17c08763bbe206640ad
Prateek <prateekmahajan03@gmail.com>
2024-07-17 19:38:57 +0530

    Initial commit

---
+++ app2-portfolio/venv/send_email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+
+def send_email(message):
+
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "prateekmahajan221@gmail.com"
+    password = "huqk ihko sxrw iqir"
+
+    receiver = "prateekmahajan03@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
