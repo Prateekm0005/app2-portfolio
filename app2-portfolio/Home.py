5dccbc55481cf82155840bf5ee8e6b81c7f1f0c2
Prateek <prateekmahajan03@gmail.com>
2024-07-17 18:18:21 +0530

    Add navigation menu and contact us page

--- app2-portfolio/Home.py
+++ app2-portfolio/Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/profile.jpeg", width=650)
+    st.image("images/profile.jpeg", width=450)
 
 with col2:
     st.title("Prateek Mahajan")
