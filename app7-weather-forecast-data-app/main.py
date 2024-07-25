8d619411dbc10a2b2521dbaea9c4b57355af7996
Prateek <prateekmahajan03@gmail.com>
2024-07-26 00:36:30 +0530

    create fake weather forecast graph dynamically

--- app7-weather-forecast-data-app/main.py
+++ app7-weather-forecast-data-app/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather forecast for the next days")
 
@@ -7,3 +8,15 @@ days = st.slider("Forecasted Days:", min_value=1, max_value=5, help="Select the 
 option = st.selectbox("Select data to view:", ("Temperature", "Sky"), help="Select the option")
 
 st.subheader(f"{option} for the next {days} days in {places} ")
+
+def get_data(days):
+    dates = ["2024-26-07", "2024-27-07", "2024-28-07"]
+    temperatures = [24,25,27]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+
+st.plotly_chart(figure)
